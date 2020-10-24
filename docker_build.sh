14bb711f7366cb8025dc38b1638be47284e695a5
YoNyeoSeok <yys8646@postech.ac.kr>
2020-10-24 20:16:39 +0000

    SPLIT build push & local devel (add local user)

--- docker_build.sh
+++ docker_build.sh
@@ -1,23 +1,15 @@
-IMAGE=yonyeoseok/conda3:cuda9.0-cudnn7-devel-ubuntu16.04
 NAME=crst
 
-docker run -idt --name tmp $IMAGE
-# create conda env
-docker exec tmp conda update -n base -c defaults conda
-docker exec tmp \
-	conda create -yn py27torch04 \
-	python=2.7 pytorch=0.4.0 torchvision=0.2.1 py-opencv=3.4 cuda90 \
-    packaging scipy h5py pyyaml matplotlib \
-	-c conda-forge -c pytorch
-# add user
-docker exec tmp apt-get update --fix-missing
-ddocker exec tmp apt-get install -y sudo git vim screen ssh libgl1-mesa-glx
-docker exec tmp apt-get clean
-docker exec tmp useradd -m -G sudo,conda $NAME
-docker exec tmp /bin/bash -c "echo $NAME:passwd | chpasswd"
-docker stop tmp
+IMAGE=yonyeoseok/$NAME:devel
+docker build -t $IMAGE .
+docker push $IMAGE
 
-docker commit tmp yonyeoseok/$NAME:devel
-docker rm tmp
-
-docker push yonyeoseok/$NAME:devel
+# add local user
+docker run --rm -idt --name $NAME $IMAGE
+docker exec $NAME apt-get update --fix-missing
+docker exec $NAME apt-get install -y sudo git vim screen ssh
+docker exec $NAME apt-get clean
+docker exec $NAME useradd -m -u $UID -G sudo,conda $NAME
+docker exec $NAME /bin/bash -c "echo $NAME:passwd | chpasswd"
+docker commit $NAME $NAME:devel
+docker stop $NAME
--- docker_run.sh
+++ docker_run.sh
@@ -4,9 +4,9 @@ CODEDIR=$HOME/research/CRST
 DATADIR=$HOME/data
 WORKDIR=/home/crst
 
-docker run -idt -u $NAME -w $WORKDIR -e "TERM=$TERM" \
+docker run -idt -w $WORKDIR -e "TERM=$TERM" \
 	--name $NAME --pid host \
 	-v=$CODEDIR:$WORKDIR/CRST \
 	--gpus all --shm-size 32g \
 	-v=$DATADIR:$WORKDIR/data:ro \
-	$IMAGE
+	$NAME:devel /bin/bash -c "service ssh restart && /bin/bash"
