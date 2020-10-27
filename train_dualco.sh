2d3ae52e1d63b284378e77361d931799653c35c7
YoNyeoSeok <yys8646@postech.ac.kr>
2020-10-27 15:31:00 +0000

    UPDATE train_dualco with remove prob argument

--- train_dualco.sh
+++ train_dualco.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python crst_seg.py --random-mirror --random-scale --rm-prob --test-flipping --save results/cbst --data-src-dir DATA_SRC_DIR --data-tgt-dir DATA_TGT_DIR
+CUDA_VISIBLE_DEVICES=0 python train_dualco.py --random-mirror --random-scale --test-flipping --save results/cbst --data-src-dir DATA_SRC_DIR --data-tgt-dir DATA_TGT_DIR
